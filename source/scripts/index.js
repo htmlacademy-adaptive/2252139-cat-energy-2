e062e6f623605c447d854f8596b6088cc93a8eba
Musa-Ismailov <164714080+Musa-Ismailov@users.noreply.github.com>
2025-04-10 05:27:28 +0300

    переделывает скрипт

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,13 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav__toggle--on')) {
+    navMain.classList.remove('main-nav__toggle--on');
+    navMain.classList.add('main-nav__toggle--off');
+  } else {
+    navMain.classList.add('main-nav__toggle--on');
+    navMain.classList.remove('main-nav__toggle--off');
+  }
+});
