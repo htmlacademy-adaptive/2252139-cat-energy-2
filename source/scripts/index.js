00a32210df26f2c76c824733113c47ad386df088
Musa-Ismailov <164714080+Musa-Ismailov@users.noreply.github.com>
2025-04-12 03:23:03 +0300

    дает состояние элементом на главной и странице каталога

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,11 +3,6 @@ const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav__toggle--on')) {
-    navMain.classList.remove('main-nav__toggle--on');
-    navMain.classList.add('main-nav__toggle--off');
-  } else {
-    navMain.classList.add('main-nav__toggle--on');
-    navMain.classList.remove('main-nav__toggle--off');
-  }
+  navMain.classList.toggle('main-nav--on');
+  navMain.classList.toggle('main-nav--off');
 });
