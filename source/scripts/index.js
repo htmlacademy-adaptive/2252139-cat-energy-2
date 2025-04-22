b0bfce4b7c18e351aceff58e29f03125cf448ab8
Musa-Ismailov <164714080+Musa-Ismailov@users.noreply.github.com>
2025-04-22 15:01:12 +0300

    делает правки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,14 @@
 /* в этот файл добавляет скрипты*/
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
+const Location = document.querySelector('.location');
 
 navMain.classList.remove('main-nav--nojs');
+Location.classList.remove('location--nojs');
+
 
 navToggle.addEventListener('click', () => {
   navMain.classList.toggle('main-nav--on');
   navMain.classList.toggle('main-nav--off');
 });
+
